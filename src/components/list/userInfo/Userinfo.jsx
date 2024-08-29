baa3fcf69bebb10f3a7de53a4311b11132d27ced
Nicolás <nicolassebastiandelrio@gmail.com>
2024-08-29 19:16:10 -0300

    changed link img

--- src/components/list/userInfo/Userinfo.jsx
+++ src/components/list/userInfo/Userinfo.jsx
@@ -3,7 +3,10 @@ import "./userInfo.css"
 const Userinfo = () => {
     return (
         <div className='userInfo'>
-            <div className="user"></div>
+            <div className="user">
+                <img src="./avatar.png" alt="" />
+                <h2>Nico del Rio</h2>
+            </div>
             <div className="icons">
                 <img src="./more.png" alt="" />
                 <img src="./video.png" alt="" />
