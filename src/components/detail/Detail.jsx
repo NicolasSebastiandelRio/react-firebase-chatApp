da2ecee4b3ec4949274acc7c21151ec447d166ad
Nicolás <nicolassebastiandelrio@gmail.com>
2024-09-04 07:45:04 -0300

    Advanced with detail sector of webpage (both jsx and css)

--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -2,9 +2,76 @@ import "./detail.css"
 
 const Detail = () => {
     return (
-    <div className='detail'>
-        Detail  
-    </div>
+        <div className='detail'>
+            <div className="user">
+                <img src="./avatar.png" alt="" />
+                <h2>Jane doe</h2>
+                <p>Lorem, ipsum dolor sit amet.</p>
+            </div>
+            <div className="info">
+                <div className="option">
+                    <div className="title">
+                        <span>Chat Settings</span>
+                        <img src="./arrowUp.png" alt="" />
+                    </div>
+                </div>
+                <div className="option">
+                    <div className="title">
+                        <span>Chat Settings</span>
+                        <img src="./arrowUp.png" alt="" />
+                    </div>
+                </div>
+                <div className="option">
+                    <div className="title">
+                        <span>Privacy & help</span>
+                        <img src="./arrowUp.png" alt="" />
+                    </div>
+                </div>
+                <div className="option">
+                    <div className="title">
+                        <span>Shared photos</span>
+                        <img src="./arrowDown.png" alt="" />
+                    </div>
+                    <div className="photos">
+                        <div className="photoItem">
+                            <div className="photoDetail">
+                                <img src="https://www.pexels.com/es-es/video/resfriado-frio-nieve-nevar-20659487/" alt="" />
+                                <span>photo_2024.png</span>
+                            </div>
+                            <img src="./download.png" alt="" className="icon" />
+                        </div>
+                        <div className="photoItem">
+                            <div className="photoDetail">
+                                <img src="https://www.pexels.com/es-es/video/resfriado-frio-nieve-nevar-20659487/" alt="" />
+                                <span>photo_2024.png</span>
+                            </div>
+                            <img src="./download.png" alt="" className="icon"/>
+                        </div>
+                        <div className="photoItem">
+                            <div className="photoDetail">
+                                <img src="https://www.pexels.com/es-es/video/resfriado-frio-nieve-nevar-20659487/" alt="" />
+                                <span>photo_2024.png</span>
+                            </div>
+                            <img src="./download.png" alt="" className="icon"/>
+                        </div>
+                        <div className="photoItem">
+                            <div className="photoDetail">
+                                <img src="https://www.pexels.com/es-es/video/resfriado-frio-nieve-nevar-20659487/" alt="" />
+                                <span>photo_2024.png</span>
+                            </div>
+                            <img src="./download.png" alt="" className="icon"/>
+                        </div>
+                    </div>
+                </div>
+                <div className="option">
+                    <div className="title">
+                        <span>Shared Files</span>
+                        <img src="./arrowUp.png" alt="" />
+                    </div>
+                </div>
+                <button>Block User</button>
+            </div>
+        </div>
     )
 }
 
